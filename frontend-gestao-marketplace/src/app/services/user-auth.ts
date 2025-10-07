588fa200d53336044ac54b78577bd3cd24af6bd0
mateussilva01 <mateussilva.dev01@gmail.com>
2025-10-07 20:43:32 -0300

    Aula 02: roteamento e autenticação.

---
+++ frontend-gestao-marketplace/src/app/services/user-auth.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserAuthService {
+  getUserToken() {
+    return localStorage.getItem('auth-token') || '';
+  }
+
+  setUserToken(token: string) {
+    localStorage.setItem('auth-token', token);
+  }
+  
+}
